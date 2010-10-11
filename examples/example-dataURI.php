f3ca8930466c59fb7ebe1aa565f3c79ef89c268a
Anton Ruban <ruban.anton@gmail.com>
2010-10-11 12:39:25 +0400

    ---

--- examples/example-dataURI.php
+++ examples/example-dataURI.php
@@ -10,47 +10,11 @@ $Resources
 	->addFile('css/background-inline.css')
 	->handler(function($string, $config) {
 		$dataURI = new dataURI($string);
-		$GLOBALS['tmp'] = $dataURI->getMhtmlContent();
-		return $dataURI->getProcessedContent();
+		return '/*' . "\r\n" . $dataURI->getMhtmlContent() . '*/' . $dataURI->getProcessedContent();
 	})
-	->handler(function($string) {
-		return '/*' . "\r\n" . $GLOBALS['tmp'] . '*/' . CSS2JavaScript($string);
-	})
-	->inline();
-
-$Resources = new ResourceCollection('JavaScript');
-$CSS = $Resources
-	->driver('CSS')
-	->addFile('css/background-inline.css')
-	->handler(function($string, $config) {
-//		$dataURI = new dataURI($string, array('MHTML_path' => 'tmp/data.css'));
-//		$dataURI->saveMHTML('tmp/data.css');
-		$dataURI = new dataURI($string);
-//		$dataURI->saveMHTML('tmp/data.css');
-		$GLOBALS['tmp'] = $dataURI->getMhtmlContent();
-		return $dataURI->getProcessedContent();
-		//return '/*' . $dataURI->getMhtmlContent() . '*/' . $dataURI->getProcessedContent();
-	})
-	->handler(function($string) {
-		return '/*' . "\r\n" . $GLOBALS['tmp'] . '*/' . CSS2JavaScript($string);
-	})
-	->file('tmp/background-inline.cache.css')
 	->inline()
-	->driver('JavaScript');
-
-
-$Resources = new ResourceCollection('CSS');
-$Resources
-	->driver('JavaScript')
-	->addFile('css/background-inline.css')
-	->handler(function($string) {
-		$dataURI = new dataURI($string, array('MHTML_path' => 'tmp/css-inline.cache.js'));//path relative to page (not CSS or JS file) URL
-		return '/*' . "\r\n" . $dataURI->getMhtmlContent() . '*/' . CSS2JavaScript($dataURI->getProcessedContent());
-	})
-	->file('tmp/css-inline.cache.js');
-//	->inline();
-
-$Resources->flush();
+	->flush()
+;
 ?>
 </head>
 	<body>
